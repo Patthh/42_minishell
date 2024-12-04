df08f069ae3e8e5a1b5c621f2649766abfd8f488
42Monkey <fortytwoworkstation@gmail.com>
2024-12-04 18:48:06 +0700

    edit header

--- include/minishell.h
+++ include/minishell.h
@@ -12,24 +12,48 @@
 
 # define PROMPT "minishell$ "
 
-// typedef enum e_token {
-// 	TOKEN_WORD,
-// 	TOKEN_REDIRECT_IN,
-// 	TOKEN_REDIRECT_OUT,
-// 	TOKEN_REDIRECT_APPEND,
-// 	TOKEN_REDIRECT_HEREDOC,
-// 	TOKEN_PIPE,
-// 	TOKEN_ENV_VAR,
-// 	TOKEN_EXIT_STATUS,
-// 	TOKEN_SPACE,
-// 	TOKEN_EOF,
-// } t_token_type;
+typedef enum e_token {
+	TOKEN_WORD,
+	TOKEN_REDIRECT_IN,			// <
+	TOKEN_REDIRECT_OUT,			// >
+	TOKEN_REDIRECT_APPEND,		// >>
+	TOKEN_REDIRECT_HEREDOC,		// <<
+	TOKEN_PIPE,					// |
+	TOKEN_ENV_VAR,				// $VAR
+	TOKEN_EXIT_STATUS,			// $?
+	TOKEN_SPACE,
+	TOKEN_EOF,
+} t_token_type;
 
-// typedef struct s_token {
-// 	t_token_type	type;
-// 	char			*string;
-// 	struct s_token	*next;
+// token structure for lexer
+typedef struct s_token {
+	t_token_type	type;
+	char			*string;
+	struct s_token	*next;
+} t_token;
 
-// } t_token;
+// redirection structure
+typedef struct s_redirection {
+	int				type;
+	char			*file;
+} t_redirection;
+
+// command structure to handle a single command
+typedef struct s_command {
+
+} t_command;
+
+// command structure to handle multiple commands
+typedef struct s_pipeline {
+
+} t_pipeline;
+
+typedef struct s_program {
+	// int				*stdout;
+	// int				*stdin;
+	// int				*stderr;
+	t_token			*token;
+	t_command		*command;
+} t_program;
 
 #endif
