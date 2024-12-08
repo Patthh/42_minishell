1623b8d6720ec20d1cf6161f5027e0cc9eff9632
42Monkey <fortytwoworkstation@gmail.com>
2024-12-08 16:53:54 +0700

    implement ft_strndup

--- source/utils/utils_libft.c
+++ source/utils/utils_libft.c
@@ -1,5 +1,7 @@
 #include "../../include/minishell.h"
 
+
+// fills memory area 's' with a specified byte 'c' for 'n' bytes
 void	*ft_memset(void *s, int c, size_t n)
 {
 	unsigned char	*temp;
@@ -12,6 +14,7 @@ void	*ft_memset(void *s, int c, size_t n)
 	return (s);
 }
 
+// compares two strings
 int	ft_strcmp(const char *s1, const char *s2)
 {
 	size_t	i;
@@ -22,4 +25,49 @@ int	ft_strcmp(const char *s1, const char *s2)
 		i++;
 	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+// duplicates the first 'n' character of 'src' into 'dest'
+char	*ft_strndup(const char *src, size_t n)
+{
+	size_t	i;
+	size_t	j;
+	char	*dest;
+
+	i = 0;
+	j = 0;
+	while (s[i] != '\0' && i < n)
+		i++;
+	dest = malloc(i + 1);
+	if (!dest)
+		return (NULL);
+	while (j < i)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+// finds the first occurence of a character in a string
+// returns a pointer to 'c' if found or NULL if not
+char	*ft_strchr(const char *string, int c)
+{
+	char	target;
+
+	target = (char)c;
+	while (*string)
+	{
+		if (*string == target)
+		{
+			return ((char *)string);
+		}
+		string++;
+	}
+	if (target == '\0')
+	{
+		return ((char *)string);
+	}
+	return (NULL);
 }
