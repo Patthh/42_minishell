4109e1651b02ba3a50bf518e70f6e38e4d04df6e
42Monkey <fortytwoworkstation@gmail.com>
2024-12-08 17:38:30 +0700

    add ft_isalnum in utils_libft.c

--- source/utils/utils_libft.c
+++ source/utils/utils_libft.c
@@ -70,3 +70,12 @@ char	*ft_strchr(const char *string, int c)
 	}
 	return (NULL);
 }
+
+int	ft_isalnum(int c)
+{
+	if ((c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
