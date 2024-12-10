632182c8ef96c2a93c88e4d4e89d8b195b7b4cac
42Monkey <fortytwoworkstation@gmail.com>
2024-12-10 19:10:04 +0700

    remove old main

--- source/main.c
+++ source/main.c
@@ -35,37 +35,3 @@ int	main(void)
 	}
 	return (0);
 }
-
-// int main (int argc, char **argv, char **envp)
-// {
-// 	(void)argc;
-// 	(void)argv;
-// 	t_program	minishell;
-// 	t_token		*tokens;
-// 	char		*input;
-
-// 	init_minishell(&minishell, envp);
-// 	while (!minishell.exit)
-// 	{
-// 		input = readline(PROMPT);
-// 		if (input == NULL)
-// 		{
-// 			printf("\n");
-// 			break ;
-// 		}
-// 		tokens = tokenizer(input);
-// 		print_tokens(tokens);
-// 		// parsing
-// 		// execution
-// 		if (ft_strcmp(input, "exit") == 0)
-// 		{
-// 			token_free_list(tokens);
-// 			free (input);
-// 			break ;
-// 		}
-// 		free(input);
-// 		token_free_list(tokens);
-// 	}
-// 	free_minishell(&minishell);
-// 	return (0);
-// }
