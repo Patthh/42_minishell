49a3498c531c45d56adeebba43e9a212c9bfc209
42Monkey <fortytwoworkstation@gmail.com>
2024-12-12 23:55:56 +0700

    remove lines

--- source/lexer/token_utils5.c
+++ source/lexer/token_utils5.c
@@ -24,28 +24,29 @@ static void	token_exit(const char **input, t_token **head, t_program *minishell)
 	}
 }
 
-char *get_env_value(t_env *env_list, const char *key)
+char	*get_env_value(t_env *env_list, const char *key)
 {
-	t_env *current = env_list;
+	t_env	*current;
+
+	current = env_list;
 	while (current)
 	{
 		if (ft_strcmp(current->key, key) == 0)
-			return current->value;
+			return (current->value);
 		current = current->next;
 	}
-	return NULL;
+	return (NULL);
 }
 
-static void token_env(const char **input, t_token **head, t_program *minishell)
+static void	token_env(const char **input, t_token **head, t_program *minishell)
 {
-	const char *start;
-	char *env_var;
-	char *env_value;
+	const char	*start;
+	char		*env_var;
+	char		*env_value;
 
 	start = *input;
 	while (**input && (ft_isalnum(**input) || **input == '_'))
 		(*input)++;
-
 	if (start == *input)
 		return ;
 	env_var = ft_strndup(start, *input - start);
@@ -54,41 +55,34 @@ static void token_env(const char **input, t_token **head, t_program *minishell)
 		env_value = NULL;
 		if (minishell->env_list)
 			env_value = get_env_value(minishell->env_list, env_var);
-
 		if (!env_value)
 			env_value = getenv(env_var);
-
 		if (env_value)
 			token_add(head, token_new(TKN_ENV, env_value));
-
 		free(env_var);
 	}
 }
 
-void token_dollar(const char **input, t_token **head, t_program *minishell)
+void	token_dollar(const char **input, t_token **head, t_program *minishell)
 {
 	(*input)++;
-
 	while (**input && ft_isspace(**input))
 		(*input)++;
-
 	if (!**input)
 	{
 		token_add(head, token_new(TKN_WORD, "$"));
-		return;
+		return ;
 	}
-
 	if (**input == '?')
 	{
 		token_exit(input, head, minishell);
-		return;
+		return ;
 	}
-
 	if (ft_isalpha(**input) || **input == '_')
 		token_env(input, head, minishell);
 	else
 	{
 		token_add(head, token_new(TKN_WORD, "$"));
-		return;
+		return ;
 	}
 }
