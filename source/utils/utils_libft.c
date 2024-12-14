1fe424c1019ce1ad2735a0205e280a89634e4530
42Monkey <fortytwoworkstation@gmail.com>
2024-12-14 23:16:55 +0700

    add ft_strncmp

--- source/utils/utils_libft.c
+++ source/utils/utils_libft.c
@@ -245,3 +245,19 @@ char	*ft_strstr(const char *big, const char *little)
 	}
 	return (NULL);
 }
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t		i;
+
+	i = 0;
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+		{
+			return ((int)((unsigned char)s1[i] - (unsigned char)s2[i]));
+		}
+		i++;
+	}
+	return (0);
+}
