f55260d86ddea3becdf1852351437ee78474fe4c
42Monkey <fortytwoworkstation@gmail.com>
2024-12-16 23:05:34 +0700

    add test print_pipeline

---
+++ source/tester.c
@@ -0,0 +1,50 @@
+#include "../include/minishell.h"
+
+void	print_tokens(t_token *head)
+{
+	t_token	*current;
+
+	// if (!head)
+	// {
+	// 	printf("Token list is empty.\n");
+	// 	return ;
+	// }
+	current = head;
+	while (current)
+	{
+		printf("Type: %d, Value: %s\n", current->type, current->value);
+		current = current->next;
+	}
+}
+
+
+// Simple and basic print_pipeline function
+void	print_pipeline(t_pipeline *pipeline)
+{
+	t_command	*command;
+	int			i;
+	int			j;
+
+	if (!pipeline)
+	{
+		printf("Pipeline: nothing in there\n");
+		return ;
+	}
+	i = 0;
+	while (i < pipeline->cmd_count)
+	{
+		command = pipeline->commands[i];
+		printf("Command %d:\n", i + 1);
+		j = 0;
+		while (command->arguments && command->arguments[j])
+		{
+			printf("  Arg %d: %s\n", j + 1, command->arguments[j]);
+			j++;
+		}
+		if (command->input)
+			printf("  Input: %s\n", command->input->filename);
+		if (command->output)
+			printf("  Output: %s\n", command->output->filename);
+		i++;
+	}
+}
