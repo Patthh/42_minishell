c1aab8a7262d5f964d2970bdbb7e5c32ce6e1cbd
42Monkey <fortytwoworkstation@gmail.com>
2024-12-16 23:04:49 +0700

    add lldb flags

--- make.sh
+++ make.sh
@@ -1,1 +1,1 @@
-cc source/*.c source/*/*.c -o minishell -Iinclude -lreadline -Wall -Wextra -Werror -g3 -Wunused -Wunreachable-code
+cc source/*.c source/*/*.c -o minishell -Iinclude -lreadline -Wall -Wextra -Werror -g -O0 -Wunused -Wunreachable-code
