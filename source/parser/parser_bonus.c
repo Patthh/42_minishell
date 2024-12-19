26a803424c83785e8a4351d836d2389aec12699b
42Monkey <fortytwoworkstation@gmail.com>
2024-12-20 00:27:39 +0700

    add parser_and, parser_or

---
+++ source/parser/parser_bonus.c
@@ -0,0 +1,53 @@
+#include "../../include/minishell.h"
+
+// t_token	*parser_and(t_token *token, t_command **command, t_pipeline *pipeline)
+// {
+// 	t_command	*new;
+// 	t_command	**new_commands;
+// 	int			count;
+
+// 	new = create_command();
+// 	if (*command)
+// 		(*command)->next = new;
+// 	pipeline->cmd_count++;
+// 	new_commands = malloc(sizeof(t_command *) * pipeline->cmd_count);
+// 	if (!new_commands)
+// 		ft_error("Parser: AND allocation failed\n");
+// 	count = 0;
+// 	while (pipeline->commands && count < pipeline->cmd_count - 1)
+// 	{
+// 		new_commands[count] = pipeline->commands[count];
+// 		count++;
+// 	}
+// 	free(pipeline->commands);
+// 	new_commands[count] = new;
+// 	pipeline->commands = new_commands;
+// 	*command = new;
+// 	return (token->next);
+// }
+
+// t_token	*parser_or(t_token *token, t_command **command, t_pipeline *pipeline)
+// {
+// 	t_command	*new;
+// 	t_command	**new_commands;
+// 	int			count;
+
+// 	new = create_command();
+// 	if (*command)
+// 		(*command)->next = new;
+// 	pipeline->cmd_count++;
+// 	new_commands = malloc(sizeof(t_command *) * pipeline->cmd_count);
+// 	if (!new_commands)
+// 		ft_error("Parser: OR allocation failed\n");
+// 	count = 0;
+// 	while (pipeline->commands && count < pipeline->cmd_count - 1)
+// 	{
+// 		new_commands[count] = pipeline->commands[count];
+// 		count++;
+// 	}
+// 	free(pipeline->commands);
+// 	new_commands[count] = new;
+// 	pipeline->commands = new_commands;
+// 	*command = new;
+// 	return (token->next);
+// }
