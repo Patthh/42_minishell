b0b00b528aa3dbcee88c5900ac11e4b7d4f1d19a
42Monkey <fortytwoworkstation@gmail.com>
2024-12-20 14:21:57 +0700

    token_operator simplified

--- source/lexer/token_utils2.c
+++ source/lexer/token_utils2.c
@@ -1,40 +1,62 @@
 #include "../../include/minishell.h"
 
-// handles pipe and logical OR
-static void	token_pipe(const char **input, t_token **head)
+void	token_operator(const char **input, t_token **head)
 {
-	if (*(*input + 1) == '|')
+	if (**input == '|')
 	{
-		token_add(head, token_new(TKN_OR, "||"));
-		(*input) += 2;
+		if (*(*input + 1) == '|')
+		{
+			token_add(head, token_new(TKN_OR, "||"));
+			(*input) += 2;
+		}
+		else
+		{
+			token_add(head, token_new(TKN_PIPE, "|"));
+			(*input)++;
+		}
 	}
-	else
-	{
-		token_add(head, token_new(TKN_PIPE, "|"));
-		(*input)++;
-	}
-}
-
-// handle background and logical AND
-static void	token_ampersand(const char **input, t_token **head)
-{
-	if (*(*input + 1) == '&')
+	else if (**input == '&' && *(*input + 1) == '&')
 	{
 		token_add(head, token_new(TKN_AND, "&&"));
 		(*input) += 2;
 	}
-	else
-	{
-		token_add(head, token_new(TKN_BG, "&"));
-		(*input)++;
-	}
 }
 
-// handles redirection tokens
-void	token_operator(const char **input, t_token **head)
-{
-	if (**input == '|')
-		token_pipe(input, head);
-	else if (**input == '&')
-		token_ampersand(input, head);
-}
+// // handles pipe and logical OR
+// static void	token_pipe(const char **input, t_token **head)
+// {
+// 	if (*(*input + 1) == '|')
+// 	{
+// 		token_add(head, token_new(TKN_OR, "||"));
+// 		(*input) += 2;
+// 	}
+// 	else
+// 	{
+// 		token_add(head, token_new(TKN_PIPE, "|"));
+// 		(*input)++;
+// 	}
+// }
+
+// // handle background and logical AND
+// static void	token_ampersand(const char **input, t_token **head)
+// {
+// 	if (*(*input + 1) == '&')
+// 	{
+// 		token_add(head, token_new(TKN_AND, "&&"));
+// 		(*input) += 2;
+// 	}
+// 	else
+// 	{
+// 		token_add(head, token_new(TKN_BG, "&"));
+// 		(*input)++;
+// 	}
+// }
+
+// // handles redirection tokens
+// void	token_operator(const char **input, t_token **head)
+// {
+// 	if (**input == '|')
+// 		token_pipe(input, head);
+// 	else if (**input == '&')
+// 		token_ampersand(input, head);
+// }
