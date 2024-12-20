947713e9aa0d645f63f056f9c84aa4399a719eaa
42Monkey <fortytwoworkstation@gmail.com>
2024-12-20 16:26:29 +0700

    updated parser

--- source/parser/parser_utils2.c
+++ source/parser/parser_utils2.c
@@ -21,29 +21,8 @@ static int	redir_filename(t_token *token)
 {
 	if (!token || token->type != TKN_WORD)
 	{
-		ft_putstr_fd("minishell: syntax error near unexpected token `newline'\n", STDERR_FILENO);
+		ft_putstr_fd("minishell: syntax error near unexpected token\n", STDERR_FILENO);
 		return (0);
-	}
-	return (1);
-}
-
-static int	redir_duplicate(t_command *command, int index)
-{
-	t_redirection	*existing;
-	const char		*types[] = {"IN", "OUT", "APPEND", "HEREDOC"};
-
-	if (index == 0 || index == 3)
-		existing = command->input;
-	else
-		existing = command->output;
-	while (existing)
-	{
-		if (ft_strcmp(existing->type, types[index]) == 0)
-		{
-			ft_putstr_fd("minishell: duplicate redirection\n", STDERR_FILENO);
-			return (0);
-		}
-		existing = existing->next;
 	}
 	return (1);
 }
@@ -52,16 +31,19 @@ static int	redir_duplicate(t_command *command, int index)
 // output: OUT or APPEND
 static void	redir_add(t_command *command, t_redirection *redirection, int index)
 {
-	if (index == 0 || index == 3)
-	{
-		redirection->next = command->input;
-		command->input = redirection;
-	}
+	t_redirection	**list;
+	// t_redirection	*temp;
+	if (index == 0)
+		list = &command->input;
+	else if (index == 1)
+		list = &command->output;
+	else if (index == 2)
+		list = &command->append;
 	else
-	{
-		redirection->next = command->output;
-		command->output = redirection;
-	}
+		list = &command->heredoc;
+	while (*list)
+		list = &(*list)->next;
+	*list = redirection;
 }
 
 // advances to the next token and check
@@ -71,16 +53,45 @@ t_token	*parser_redirection(t_token *token, t_command *command)
 {
 	const char		*types[] = {"INT", "OUT", "APPEND", "HEREDOC"};
 	t_redirection	*redirection;
+	t_token			*next;
 	int				index;
 
 	index = redir_index(token->type);
 	token = token->next;
 	if (!redir_filename(token))
 		return (NULL);
-	if (!redir_duplicate(command, index))
-		return (NULL);
+	next = token->next;
+	if (next && (next->type == TKN_OUT || next->type == TKN_RDA))
+	{
+		if (!parser_argument(command, token->value))
+			ft_error("Parser: Failed to add argument\n");
+		return (token->next);
+	}
 	redirection = create_redirection(types[index], token->value);
+	if (!redirection)
+		return (NULL);
 	redir_add(command, redirection, index);
 	return (token->next);
 }
 
+int	parser_sequence(t_token *tokens)
+{
+	t_token	*token;
+
+	token = tokens;
+	while (token)
+	{
+		if (token->type == TKN_PIPE && (!token->next || token->next->type == TKN_PIPE))
+		{
+			ft_putstr_fd("minishell: syntax error near unexpected token `|'\n", STDERR_FILENO);
+			return (0);
+		}
+		if ((token->type == TKN_IN || token->type == TKN_OUT || token->type == TKN_RDA || token->type == TKN_RDH) && (!token->next || token->next->type != TKN_WORD))
+		{
+			ft_putstr_fd("minishell: syntax error near unexpected token `newline'\n", STDERR_FILENO);
+			return (0);
+		}
+		token = token->next;
+	}
+	return (1);
+}
