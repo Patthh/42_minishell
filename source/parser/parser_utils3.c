290e10670b031824a26360564b41f374101f4186
42Monkey <fortytwoworkstation@gmail.com>
2025-01-10 21:30:17 +0700

    complete exit status in parser_sequence

--- source/parser/parser_utils3.c
+++ source/parser/parser_utils3.c
@@ -68,7 +68,7 @@ t_token	*parser_status(t_token *token, t_program *minishell)
 	return (token);
 }
 
-int	parser_sequence(t_token *tokens)
+int	parser_sequence(t_token *tokens, t_program *minishell)
 {
 	t_token	*token;
 
@@ -78,11 +78,13 @@ int	parser_sequence(t_token *tokens)
 		if (token->type == TKN_PIPE && (!token->next || token->next->type == TKN_PIPE))
 		{
 			ft_putstr_fd("minishell: syntax error near unexpected token `|'\n", STDERR_FILENO);
+			minishell->status = 2;
 			return (0);
 		}
 		if ((token->type == TKN_IN || token->type == TKN_OUT || token->type == TKN_RDA || token->type == TKN_RDH) && (!token->next || token->next->type != TKN_WORD))
 		{
 			ft_putstr_fd("minishell: syntax error near unexpected token `newline'\n", STDERR_FILENO);
+			minishell->status = 2;
 			return (0);
 		}
 		token = token->next;
